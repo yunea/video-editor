d9484f5f1bd1a0d5be30f271b4c034602c37c319
aina <code.experience01@gmail.com>
2025-05-21 16:14:08 +0200

    add dockerignore

---
+++ edit_video.py
@@ -0,0 +1,26 @@
+import whisper
+import subprocess
+import sys
+import os
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+print(f"📥 Traitement de la vidéo : {input_path}")
+
+model = whisper.load_model("base")
+result = model.transcribe(input_path)
+print("🧠 Transcription terminée.")
+
+# Enlever les silences avec FFmpeg
+command = [
+    "ffmpeg",
+    "-i", input_path,
+    "-af", "silenceremove=start_periods=1:start_duration=0.5:start_threshold=-35dB",
+    "-c:v", "copy",
+    output_path
+]
+
+print(f"✂️ Montage en cours : {output_path}")
+subprocess.run(command)
+print("✅ Montage terminé.")
